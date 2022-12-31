526930b79f73096f2aa4747b7f291d6c8c9eee16
sayfelanjos <saymonfelipe@hotmail.com>
2022-12-31 15:57:53 -0300

    Using Pipes for Validation

--- messages/dist/main.js
+++ messages/dist/main.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
+const common_1 = require("@nestjs/common");
 const messages_module_1 = require("./messages/messages.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(messages_module_1.MessagesModule);
+    app.useGlobalPipes(new common_1.ValidationPipe());
     await app.listen(3000);
 }
-bootstrap();
+bootstrap().then(r => r);
 //# sourceMappingURL=main.js.map
