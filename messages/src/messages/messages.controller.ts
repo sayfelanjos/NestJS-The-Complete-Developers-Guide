0128b9950452b0ba50d3a0a83e3ab63bb6a08ee1
sayfelanjos <saymonfelipe@hotmail.com>
2022-12-31 18:41:24 -0300

    Implementing a Repository

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
-import { CreateMessageDto } from "../../dtos/create-message.dto";
+import { CreateMessageDto } from "./dtos/create-message.dto";
 
 @Controller("/messages")
 export class MessagesController {
---
+++ messages/src/messages/messages.repository.ts
@@ -0,0 +1,22 @@
+import { readFile, writeFile } from "fs/promises";
+
+export class MessagesRepository {
+  async findOne(id: string) {
+    const contents = await readFile("messages.json", "utf-8");
+    const messages = JSON.parse(contents);
+    return messages[id];
+  }
+
+  async findAll() {
+    const contents = await readFile("messages.json", "utf-8");
+    return JSON.parse(contents);
+  }
+
+  async create(content: string) {
+    const contents = await readFile("messages.json", "utf-8");
+    const messages = JSON.parse(contents);
+    const id = Math.floor(Math.random() * 900);
+    messages[id] = { id, content };
+    await writeFile("messages.json", JSON.stringify(messages));
+  }
+}
