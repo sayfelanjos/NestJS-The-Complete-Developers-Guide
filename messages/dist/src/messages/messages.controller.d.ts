ecd7782fa713e6c19d6a4e9235bc232be2d88611
sayfelanjos <saymonfelipe@hotmail.com>
2022-12-31 19:17:48 -0300

    Update

--- messages/dist/src/messages/messages.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { CreateMessageDto } from "../../dtos/create-message.dto";
-export declare class MessagesController {
-    listMessages(): void;
-    createMessage(body: CreateMessageDto): void;
-    getMessage(id: string): void;
-}
