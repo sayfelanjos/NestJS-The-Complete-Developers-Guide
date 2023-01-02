ca2a07611b5ff9085ebc6db24ceb10d0e880bb93
sayfelanjos <saymonfelipe@hotmail.com>
2023-01-02 19:25:00 -0300

    Understanding Inversion of Control

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -4,26 +4,22 @@ import { MessagesService } from "./messages.service";
 
 @Controller("/messages")
 export class MessagesController {
-  messageService: MessagesService;
-
-  constructor() {
-    // DON'T DO THIS ON REAL APP, INSTEAD USE DEPENDENCY INJECTION
-    this.messageService = new MessagesService();
+  constructor(public messagesService: MessagesService) {
   }
 
   @Get()
   listMessages() {
-    return this.messageService.findAll();
+    return this.messagesService.findAll();
   };
 
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
-    return this.messageService.create(body.content);
+    return this.messagesService.create(body.content);
   }
 
   @Get("/:id")
   async getMessage(@Param("id") id: string) {
-    const message = await this.messageService.findOne(id);
+    const message = await this.messagesService.findOne(id);
     if (!message) {
       throw new NotFoundException("message not found");
     }
