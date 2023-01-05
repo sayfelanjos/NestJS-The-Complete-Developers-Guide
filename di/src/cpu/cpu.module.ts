0254a1a59f1a49f3012a1ebff3a49f8fc4a076d6
sayfelanjos <saymonfelipe@hotmail.com>
2023-01-05 11:52:37 -0300

    Setting-Up-DI-Between-Modules

--- di/src/cpu/cpu.module.ts
+++ di/src/cpu/cpu.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CpuService } from './cpu.service';
+import { PowerModule } from "../power/power.module";
 
 @Module({
+  imports: [PowerModule],
   providers: [CpuService]
 })
 export class CpuModule {}
--- di/src/cpu/cpu.service.ts
+++ di/src/cpu/cpu.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from "../power/power.service";
 
 @Injectable()
-export class CpuService {}
+export class CpuService {
+  constructor(private powerService: PowerService) {};
+}
